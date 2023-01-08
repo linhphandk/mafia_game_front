6d50289182145414fa2a010de7ce2ec6345d79bf
linh <thachlinhphan@gmail.com>
2023-01-08 14:57:28 +0000

    add user service

---
+++ lib/Services/user_service.dart
@@ -0,0 +1,25 @@
+import 'package:mafia_game_front/Validators/user_validator.dart';
+
+import '../Entities/user.dart';
+
+class UserService {
+  User? createUser(
+      String email, String username, String password, String repeatPassword) {
+    if (validateEmail(email) != null) {
+      return null;
+    }
+
+    if (validateUsername(username) != null) {
+      return null;
+    }
+
+    if (validatePassword(password).isNotEmpty) {
+      return null;
+    }
+
+    if (password != repeatPassword) {
+      return null;
+    }
+    return User(email, username, password);
+  }
+}
